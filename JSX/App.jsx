a2d90cb0f751c8a4534ecd02f4630d0d4c0eb784
JeanLouPatate <comingeslouis51@gmail.com>
2025-08-15 22:35:54 +0200

    Version finale

---
+++ JSX/App.jsx
@@ -0,0 +1,13 @@
+import Hero from "./components/Hero";
+import "./"
+import React from "./hero.css";
+function App() {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
+
